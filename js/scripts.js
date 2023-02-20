d9c1ebf9368d7693869dcc1021d1bee25185816d
Lananh Ho <122191957+laniho@users.noreply.github.com>
2023-02-19 20:11:06 -0600

    add space in front of 'responsive'

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@ console.log("Hello world");
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
     if (x.className === 'navtoggle') {
-       x.className += 'responsive'; 
+      x.className += ' responsive';
     } else {
-        x.className = 'navtoggle';
+      x.className = 'navtoggle';
     }
 }
