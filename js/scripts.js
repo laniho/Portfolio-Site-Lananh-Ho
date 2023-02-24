f1fbc0c965fe3cffcfa5e759295756f9e44798f1
Lananh Ho <122191957+laniho@users.noreply.github.com>
2023-02-24 12:59:16 -0600

    Fix coding errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 console.log("Hello world");
 
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
+    var x = document.getElementById('myNavtoggle'); /*searches document for ID; assign as variable x*/
     if (x.className === 'navtoggle') {
-      x.className += ' responsive';
-    } else {
-      x.className = 'navtoggle';
+      x.className += ' responsive'; /*adds responsive classname to x (show menu)*/
+    } else { /*if class name is currently navtoggle responsive*/
+      x.className = 'navtoggle'; /*change className back to navtoggle*/
     }
 }
