d74c322cec6cc8ed44aabc1089288d8eaee51fad
Lananh Ho <122191957+laniho@users.noreply.github.com>
2023-02-28 20:02:09 -0600

    add mobile breakpoint condition

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,8 @@ console.log("Hello world");
 
 function menuToggle() {
     var x = document.getElementById('myNavtoggle'); /*searches document for ID; assign as variable x*/
-    if (x.className === 'navtoggle') {
+    var isMobileBreakpoint = window.innerWidth <= 640
+    if (x.className === 'navtoggle' && isMobileBreakpoint) {
       x.className += ' responsive'; /*adds responsive classname to x (show menu)*/
     } else { /*if class name is currently navtoggle responsive*/
       x.className = 'navtoggle'; /*change className back to navtoggle*/
